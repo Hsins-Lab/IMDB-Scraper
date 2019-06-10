8efd40ecfdc631e42e9c6b1632d32729baa534c0
Hsins <hsinspeng@gmail.com>
2019-06-10 17:45:36 +0800

    Refactoring.

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const requestPromise = require('request-promise');
 const Json2csvParser = require('json2csv').Parser;
 
 const URLS = [
-  'https://www.imdb.com/title/tt6565702/?ref_=rvi_tt', 
-  'https://www.imdb.com/title/tt5114356/?ref_=nm_knf_t3'
+  'https://www.imdb.com/title/tt6565702/', 
+  'https://www.imdb.com/title/tt5114356/',
+  'https://www.imdb.com/title/tt0111161/',
+  'https://www.imdb.com/title/tt0068646/',
+  'https://www.imdb.com/title/tt0071562/',
+  'https://www.imdb.com/title/tt0108052/',
 ];
+
+const HEADER = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,la;q=0.5',
+  'Cache-Control': 'max-age=0',
+  'Connection': 'keep-alive',
+  'Upgrade-Insecure-Requests': '1',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+};
 
 (async () => {
   let moviesData = [];
@@ -15,19 +29,11 @@ const URLS = [
   for (let movie of URLS) {
     const response = await requestPromise({
       uri: movie,
-      headers: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,la;q=0.5',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Host': 'www.imdb.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-      },
+      headers: { ...HEADER, 'Host': 'www.imdb.com'},
       gzip: true
     });
     
+    // Data Parsing
     let $ = cheerio.load(response);
 
     let title = $('div[class="title_wrapper"] > h1').text().trim();
@@ -51,20 +57,13 @@ const URLS = [
       genres
     });
 
+    // Download Images
     let file = fs.createWriteStream(`Poster - ${title}.jpg`);
 
     await new Promise((resolve, reject) => {
       let stream = request({
         uri: poster,
-        headers: {
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-          'Accept-Encoding': 'gzip, deflate, br',
-          'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,la;q=0.5',
-          'Cache-Control': 'max-age=0',
-          'Connection': 'keep-alive',
-          'Upgrade-Insecure-Requests': '1',
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-        },
+        headers: HEADER,
         gzip: true
       })
       .pipe(file)
@@ -80,6 +79,7 @@ const URLS = [
       console.log(`${title} has an error on download. ${error}`);
     });
 
+    // Write date to files
     const json2csvParser = new Json2csvParser();
     const csv = json2csvParser.parse(moviesData);
 
